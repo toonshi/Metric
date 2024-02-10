e66997ddc4d2393bff2779d5503669a7f2f5e18a
agoyamike@gmail.com <agoyamike@gmail.comm>
2024-02-10 14:41:23 +0300

    gettting better

--- Pharm/Pharm/urls.py
+++ Pharm/Pharm/urls.py
@@ -1,4 +1,4 @@
-"""did_django_google_api_tutorial URL Configuration
+"""Pharm URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
